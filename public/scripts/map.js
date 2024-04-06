015fa04706f7509817686e5d87c7057684a7d3f1
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-05 23:56:37 -0700

    displayed map for first time

---
+++ public/scripts/map.js
@@ -0,0 +1,27 @@
+const getPosition = () => {
+  if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(loadMap, function () {
+      alert("Could not get your position");
+    });
+};
+
+const loadMap = (position) => {
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+
+  const coords = [latitude, longitude];
+
+  const map = L.map("map").setView(coords, 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Handling clicks on map
+  map.on("click", () => {
+    console.log("map was clicked");
+  });
+};
+
+getPosition();
