d5aeb54ea0fe494e49f3feb3951e0bcff18ac880
Lily <lmayhenderson@yahoo.com>
2024-04-10 12:15:49 -0700

    Test

--- db/seeds/02_maps.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO maps (title, description, user_id) VALUES ('grocery stores', 'nice place', 2);
-INSERT INTO maps (title, description, user_id) VALUES ('vancouver shops', 'nice place', 2);
