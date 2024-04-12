bf255bd5fc5c48342fd7be9e6b3738d0221c1006
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-12 08:32:37 -0700

    updated favourite button on map view

--- db/seeds/03_points.sql
+++ db/seeds/03_points.sql
@@ -26,3 +26,16 @@ VALUES
   (POINT(40.769077, -73.956617), 'Blink Fitness East Harlem', 'Friendly neighborhood gym with personal training options.', 'https://lh3.googleusercontent.com/p/AF1QipONDZ8hVl4qXIy_V_8z9S1MxTTNtbcLFJNAx0cj=s680-w680-h510', 3, 3),
   (POINT(40.742153, -73.996094), 'Barrys Bootcamp Chelsea', 'High-intensity interval training in a group setting.', 'https://lh3.googleusercontent.com/p/AF1QipONDZ8hVl4qXIy_V_8z9S1MxTTNtbcLFJNAx0cj=s680-w680-h510', 3, 3),
   (POINT(40.752834, -73.981717), 'Crunch Fitness Union Square', 'Offering a variety of workout classes and equipment.', 'https://lh3.googleusercontent.com/p/AF1QipONDZ8hVl4qXIy_V_8z9S1MxTTNtbcLFJNAx0cj=s680-w680-h510', 3, 3);
+
+
+INSERT INTO points (coords, title, description, image_url, map_id, added_by) VALUES
+  ('(35.6895, 139.6917)', 'Shinjuku Gyoen National Garden', 'A beautiful garden where you can find Grass-type Pokémon like Bulbasaur and Oddish.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c315.png', 6, 5),
+  ('(35.6586, 139.7454)', 'Tokyo Tower', 'An iconic landmark with various Electric-type Pokémon such as Pikachu and Magnemite.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c31f.png', 6, 5),
+  ('(35.7127, 139.7612)', 'Ueno Park', 'A large park where you can encounter Flying-type Pokémon like Pidgey and Spearow.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c338.png', 6, 5),
+  ('(35.7101, 139.8107)', 'Tokyo Skytree', 'An observation tower with sightings of Flying-type Pokémon such as Murkrow and Starly.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c33d.png', 6, 5),
+  ('(35.6894, 139.6981)', 'Yoyogi Park', 'A popular park where you can find Bug-type Pokémon like Caterpie and Weedle.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c30f.png', 6, 5),
+  ('(35.6581, 139.7005)', 'Meiji Shrine', 'A serene shrine with sightings of Psychic-type Pokémon such as Abra and Drowzee.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c339.png', 6, 5),
+  ('(35.7090, 139.7322)', 'Akihabara', 'A vibrant district where you can encounter Electric-type Pokémon like Voltorb and Electabuzz.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c324.png', 6, 5),
+  ('(35.6938, 139.7034)', 'Harajuku', 'A trendy area with sightings of Fairy-type Pokémon such as Jigglypuff and Clefairy.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c32b.png', 6, 5),
+  ('(35.7100, 139.8107)', 'Sumida Aquarium', 'An aquarium with Water-type Pokémon like Magikarp and Wailmer.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c326.png', 6, 5),
+  ('(35.6706, 139.7690)', 'Imperial Palace', 'A historic site with sightings of Dragon-type Pokémon such as Dratini and Dragonair.', 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c323.png', 6, 5);
